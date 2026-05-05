374bffd23aec73e91d729b48fa144e14d8d148bd
ntnbinh <80180304+ntnbinh@users.noreply.github.com>
2026-05-05 01:33:54 -0700

    fix mismatch dependencies

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -5,9 +5,13 @@ const { wrapWithReanimatedMetroConfig } = require("react-native-reanimated/metro
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.transformer.unstable_allowRequireContext = true; 
+
 const uniwindConfig = withUniwindConfig(wrapWithReanimatedMetroConfig(config), {
   cssEntryFile: "./global.css",
   dtsFile: "./uniwind-types.d.ts",
 });
+
+uniwindConfig.resolver.sourceExts = [...uniwindConfig.resolver.sourceExts, "mjs"];
 
 module.exports = uniwindConfig;
